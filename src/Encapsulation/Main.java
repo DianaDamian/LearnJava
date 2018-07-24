075d4ae0d0dfc7c78bd00005cd522b4affbc52ec
DianaDamian <diana.damian@tremend.com>
2018-07-24 18:08:57 +0300

    Uno

--- src/Encapsulation/Main.java
+++ src/Encapsulation/Main.java
@@ -5,6 +5,12 @@ public class Main {
     public static void main(String[] args) {
         Player player= new Player();
         player.name="Tim";
+        player.health=20;
+        player.weapon="Sword";
+
+        int damage=10;
+        player.loseHealth(damage);
+        System.out.println(player.healthRemaining());
 
 
     }
